bf289737da74e59e25dfe96338e101b16a40d509
Jeremy Zhou <jeremy.q.zhou@gmail.com>
2018-01-17 18:17:56 +0000

    Add intercom bot functionality. (temporary)

--- zulip_bots/zulip_bots/bots/intercom/intercom.py
+++ zulip_bots/zulip_bots/bots/intercom/intercom.py
@@ -11,8 +11,25 @@ class IntercomHandler(object):
         This bot integrates Intercom.
         '''
 
-    def handle_message(self, message: Any, bot_handler: Any) -> None:
-        content = 'beep boop' # type: str
-        bot_handler.send_reply(message, content)
+    def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
+        intercom_bot_response = get_intercom_bot_response(message)
+        bot_handler.send_reply(message, intercom_bot_response)
+
+def get_intercom_bot_response(message: Dict[str, str]) -> str:
+    content = message['content'].strip().split()
+    command = content[0]
+    if command == 'help':
+        return 'Intercom bot supports these commands: ' \
+               '\n\'@intercom help\' to show this help message.' \
+               '\n\'@intercom create-user <email> <name>\' to create a new user.' \
+               '\n\'@intercom delete-user <email> to delete a user.'
+    elif command == 'create-user':
+        intercom.users.create(email=content[1], name=content[2])
+        return 'User created.'
+    elif command == 'delete-user':
+        intercom.users.delete(intercom.users.find(email=content[1]))
+        return 'User created.'
+    else:
+        return command + ' is not a currently available command. Type \'@intercom help\' to see available commands.'
 
 handler_class = IntercomHandler
